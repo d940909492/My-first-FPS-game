f20edf54116c9ad1ad029336cc9893cafb771904
d940909492 <dong5455993@gmail.com>
2022-12-20 20:47:06 -0500

    update

--- shooting_game.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "shooting_game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, shooting_game, "shooting_game" );
